758ab68e2b2b3f7c86aeabcd6ff54fcfc467ad31
maguette03 <maguette_laye.thiaw_tall@lcieducation.net>
2024-12-13 13:26:26 -0500

    configuration des Classes DAO

---
+++ 420DA3_Examen_Final/DataAccess/AuteurDAO.cs
@@ -0,0 +1,45 @@
+﻿using _420DA3_Examen_Final.Business.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _420DA3_Examen_Final.DataAccess
+{
+    internal class AuteurDAO
+    {
+        private MyDbContext context;
+
+        public AuteurDAO(MyDbContext context)
+        {
+            this.context = context;
+        }
+
+
+        public List<Auteur> GetAll()
+        {
+            return this.context.Auteurs.ToList();
+        }
+
+        //d’obtenir, pour un livre, la liste des auteurs auquel il est associés
+        public List<Auteur> GetAuteursAssocies(Livre livre)
+        {
+            return this.context.Auteurs
+                .Include(a => a.Livres)
+                .Where(a => a.Livres.Contains(livre))
+                .ToList();
+        }
+
+        //d’obtenir, pour un livre, la liste des auteurs auquel il n’est PAS associé.
+
+        public List<Auteur> GetAuteursNoAssocies(Livre livre)
+        {
+            return this.context.Auteurs
+                .Include(a => a.Livres)
+                .Where(a => !a.Livres.Contains(livre))
+                .ToList();
+        }
+    }
+}
---
+++ 420DA3_Examen_Final/DataAccess/LivreDAO.cs
@@ -0,0 +1,46 @@
+﻿using _420DA3_Examen_Final.Business.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _420DA3_Examen_Final.DataAccess
+{
+    internal class LivreDAO
+    {
+        private MyDbContext context;
+        public LivreDAO(MyDbContext context)
+        {
+            this.context = context;
+        }
+
+        public List<Livre> Search(string criterion)
+        {
+            return this.context.livres
+                .Where(l => l.Titre.Contains(criterion) || l.Isbn.Contains(criterion))
+                .Include(l => l.Auteurs)
+                .ToList();
+        }
+
+        public Livre Insert(Livre livre)
+        {
+            _ = this.context.livres.Add(livre);
+            _ = this.context.SaveChanges();
+            return livre;
+        }
+
+        public Livre Update(Livre livre) { 
+            _= this.context.livres.Update(livre);
+            _ = this.context.SaveChanges();
+            return livre;
+        }
+
+        public Livre Delete(Livre livre) {
+            _ = this.context.livres.Remove(livre);
+            _ = this.context.SaveChanges();
+            return livre;
+        }
+    }
+}
