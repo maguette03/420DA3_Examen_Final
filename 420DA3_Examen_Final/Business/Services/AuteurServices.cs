0bf6e74f79219eb75eb8da8223cf8bcbc3a36879
maguette03 <maguette_laye.thiaw_tall@lcieducation.net>
2024-12-13 13:37:53 -0500

    Une partie de la configuration des classe services

---
+++ 420DA3_Examen_Final/Business/Services/AuteurServices.cs
@@ -0,0 +1,36 @@
+﻿using _420DA3_Examen_Final.Business.Domain;
+using _420DA3_Examen_Final.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _420DA3_Examen_Final.Business.Services
+{
+    
+    internal class AuteurServices
+    {
+        private AuteurDAO dao;
+        
+        public AuteurServices(MyDbContext context)
+        {
+            this.dao = new AuteurDAO(context);
+        }
+
+
+        public List<Auteur> GetAll()
+        {
+            return dao.GetAll();    
+        }
+         public  List<Auteur> GetAuteursAssocies(Livre livre)
+         {
+            return dao.GetAuteursAssocies(livre);
+         }
+
+        public List<Auteur> GetAuteursNoAssocies(Livre livre)
+        {
+            return dao.GetAuteursNoAssocies(livre);
+        }
+    }
+}
---
+++ 420DA3_Examen_Final/Business/Services/LivreServices.cs
@@ -0,0 +1,47 @@
+﻿using _420DA3_Examen_Final.Business.Domain;
+using _420DA3_Examen_Final.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _420DA3_Examen_Final.Business.Services
+{
+    internal class LivreServices
+    {
+        private LivreDAO dao;
+        public LivreServices(MyDbContext context)
+        {
+            this.dao = new LivreDAO(context);
+            //  this.view = new LivreView(parentApp);
+
+        }
+
+        public Livre? OpenViewForCreation()
+        {
+            Livre nowLivre = new Livre();
+           
+        }
+
+
+        public List<Livre> SearchLivre(string criterion)
+        {
+            return dao.Search(criterion);
+        }
+
+        public Livre InsertLivre(Livre livre) {
+            return this.dao.Insert(livre);
+        }
+
+        public Livre UpdateLivre(Livre livre)
+        {
+            return this.dao.Update(livre);
+        }
+        public Livre DeleteLivre(Livre livre)
+        {
+            return this.dao.Delete(livre);
+        }
+    }
+}
